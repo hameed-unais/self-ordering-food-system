28355fd498103c66c7ed5421022294e7a6b1ae81
Unwise <hameed.m.unais@gmail.com>
2026-07-18 22:10:56 +0800

    Added functions for getting order input and printing order summary

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Deining the menu items and prices
 food_menu = {
     "01":{"name":"Ayam Bakar Set", "transl": "Grilled Chicken Set", "price": 5.80},
     "02":{"name":"Dori Bakar Set", "transl": "Grilled Dori Set", "price": 5.90},
@@ -16,9 +17,21 @@ add_ons = {
     "A5":{"name":"Fried/Grilled Chicken Leg Quarter (per piece)", "price": 3.50,},
     "A6":{"name":"Fried Chicken Wing (1pc)", "price": 1.50,},
 }
+shopping_cart = {} #storing the items user wants to order
+
+#testing out functions before aking them into definitions
 print("Penyet + BBQ Set Meal")
 for item_no, item in food_menu.items():
     print(f"\n[{item_no}]/\t{item['name']:<30} : ${item['price']:.2f} \n\t{item['transl']}")
 print("\nAdd-ons")
 for add_on_no, add_on in add_ons.items():
     print(f"\n{add_on_no}/\t{add_on['name']:<30} : ${add_on['price']:.2f}")
+
+input_item = input('\nPlease enter the item number you want to order (e.g., 01, 02, A1, A2):')
+if input_item in food_menu:
+    shopping_cart[input_item] = food_menu[input_item]
+else:
+    print("Invalid item number. Please try again.")
+print(shopping_cart)
+cart_total = sum(item['price'] for item in shopping_cart.values())
+print(f"Total: ${cart_total:.2f}")
